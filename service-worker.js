72a04b6a0ea20e4809ca5d8c0ecdb968ef01579c
Davide Boschetti <bdavide1999@gmail.com>
2026-05-05 22:13:53 +0200

    Update cache version in service worker

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // === cambia SOLO questa riga per forzare un refresh completo ===
-const CACHE = 'vidgrabber-19-04-2026-1';
+const CACHE = 'vidgrabber-05-05-2026-1';
 // ==============================================================================
 
 const ASSETS = [
